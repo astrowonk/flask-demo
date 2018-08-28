b878a2a1ece10c7e9cf7b5ab9083d2e5d9a7b806
Marcos <sirmarcos@gmail.com>
2018-08-28 10:10:46 -0400

    handle errors for bad ticker symbol. make ticker symbols uppercase

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ from bokeh.models import ColumnDataSource
 import pandas as pd
 app = Flask(__name__)
 
-
 def getStockInfo(ticker):
 
   queryString = "https://api.iextrading.com/1.0/stock/" + ticker + "/chart/1y"
-  myDict = requests.get(queryString).json()
-  myData = pd.DataFrame.from_dict(myDict)
-
+  myRaw = requests.get(queryString)
+  if myRaw.status_code == 200:
+    myData = pd.DataFrame.from_dict(myRaw.json())
+    myData['date'] = pd.to_datetime(myData['date'])
+  else:
+    myData = pd.DataFrame()
   return(myData)
+
 
 
 @app.route('/',methods=['GET','POST'])
@@ -27,17 +30,24 @@ def index():
     aTicker='TSLA'
 
   myData = getStockInfo(aTicker)
-  myData['date'] = pd.to_datetime(myData['date'])
-  fig = figure(plot_width=800, plot_height=600,x_axis_type='datetime')
-  source = ColumnDataSource(data=myData)
 
-  fig.circle(x='date', y='close', source=source)
-  script, div = components(fig)
+  if myData.size == 0:
+    errorText = "No Stock Price Data for: " + aTicker.upper()
+    script = ""
+    div = ""
+  else:
+    errorText = ""
+    fig = figure(plot_width=800, plot_height=600, x_axis_type='datetime')
+    source = ColumnDataSource(data=myData)
+
+    fig.circle(x='date', y='close', source=source)
+    script, div = components(fig)
+
   if request.method == 'GET':
-    return render_template('index.html',script=script, div=div, text=aTicker)
+    return render_template('index.html',script=script, div=div, text=aTicker.upper(),errorText=errorText)
   else:
     aTicker = request.form.get('symbol')
-    return render_template('index.html',script=script, div=div, text=aTicker)
+    return render_template('index.html',script=script, div=div, text=aTicker.upper(), errorText=errorText)
 
 
 @app.route('/about')
